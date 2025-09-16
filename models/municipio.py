6b3624757f4623b80b28f266c8efbca8d502efaf
InkureAlicante <158462906+InkureAlicante@users.noreply.github.com>
2025-09-16 20:04:04 +0200

    municipio.py

--- models/municipio.py
+++ models/municipio.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 class Municipio:
-    def __init__(self, name, history, inhabitants, affiliates, events, info_tables, coordinador, vice_coordinator, team_members):
+    def __init__(self, name, history, inhabitants, affiliates, events, info_tables, coordinador, LISTA, team_members):
         self.name = name
         self.history = history
         self.inhabitants = inhabitants
@@ -8,7 +8,7 @@ class Municipio:
         self.events = events
         self.info_tables = info_tables
         self.coordinador = coordinador
-        self.vice_coordinator = vice_coordinator
+        self.LISTA = LISTA
         self.team_members = team_members
 
     def add_event(self, event_name):
@@ -17,3 +17,4 @@ class Municipio:
     def remove_event(self, event_name):
         if event_name in self.events:
             self.events.remove(event_name)
+
